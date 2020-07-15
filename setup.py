51760c73d0cd46b8d1eedb4822f188610aa5cc0a
ldf <liudengfeng_sd@outlook.com>
2020-07-16 00:40:53 +0800

    使用plotly绘图

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from setuptools import setup
 import versioneer
 
 DISTNAME = 'pyfolio'
-DESCRIPTION = "pyfolio is a Python library for performance and risk analysis of financial portfolios"
+DESCRIPTION = "pyfolio is a Python library for performance"
+"and risk analysis of financial portfolios"
 LONG_DESCRIPTION = """pyfolio is a Python library for performance and risk analysis of
 financial portfolios developed by `Quantopian Inc`_. It works well with the
 `Zipline`_ open source backtesting library.
@@ -37,37 +38,32 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: Scientific/Engineering :: Mathematics',
                'Operating System :: OS Independent']
 
-if (sys.version_info.major, sys.version_info.minor) >= (3, 3):
-    support_ipython_6 = True
-else:
-    support_ipython_6 = False
+support_ipython_6 = (sys.version_info >= (3, 3))
 
 install_reqs = [
     'ipython>=3.2.3' if support_ipython_6 else 'ipython>=3.2.3, <6',
     'matplotlib>=1.4.0',
     'numpy>=1.11.1',
-    'pandas>=0.23.4',
+    'pandas>=1.0.5',
     'pytz>=2014.10',
     'scipy>=0.14.0',
     'scikit-learn>=0.16.1',
     'seaborn>=0.7.1',
-    'empyrical>=0.5.0'
+    'empyrical>=0.5.0',
 ]
 
 test_reqs = ['nose>=1.3.7', 'nose-parameterized>=0.5.0', 'runipy>=0.1.3']
-bayesian_reqs = ['pymc3 >= 3.5']
 
 extras_reqs = {
-    'bayesian': bayesian_reqs,
     'test': test_reqs,
-    'all': test_reqs + bayesian_reqs,
+    'all': test_reqs
 }
 
 if __name__ == "__main__":
     setup(
         name=DISTNAME,
         cmdclass=versioneer.get_cmdclass(),
-        version='0.9.0', #versioneer.get_version(),
+        version='0.9.0', # versioneer.get_version(),
         maintainer=MAINTAINER,
         maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
